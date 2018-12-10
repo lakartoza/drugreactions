60404bb15db25c1bd4e2bd10452839af8a560743
Valentine Bondar <lakartoza@gmail.com>
2018-12-09 23:34:55 -0800

    Adding Text Input for user. Upon submission box displays text. TODO is to connect to FDA website. Only HTML template changes using vue at this time.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="centered" id="app">
+    <img alt="Vue logo" src="./assets/rx_reminder.png">
+    <HelloWorld/>
   </div>
 </template>
 
@@ -21,8 +21,12 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.centered {
+  text-align: center;
+
 }
 </style>
